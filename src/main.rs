b2d929e70e17223ced35923045a932d579587710
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2021-12-19 15:59:07 -0600

    working-ish?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use bottom::app::{self, data_harvester, DataFilters};
+
 fn main() {
-    println!("Hello, world!");
+    // let disk_filter =
+    //     get_ignore_list(&config.disk_filter).context("Update 'disk_filter' in your config file")?;
+    // let mount_filter = get_ignore_list(&config.mount_filter)
+    //     .context("Update 'mount_filter' in your config file")?;
+    // let temp_filter =
+    //     get_ignore_list(&config.temp_filter).context("Update 'temp_filter' in your config file")?;
+    // let net_filter =
+    //     get_ignore_list(&config.net_filter).context("Update 'net_filter' in your config file")?;
+
+    let disk_filter = None;
+    let mount_filter = None;
+    let temp_filter = None;
+    let net_filter = None;
+
+    let filters = DataFilters {
+        disk_filter,
+        mount_filter,
+        temp_filter,
+        net_filter,
+    };
+
+    // .filters(DataFilters {
+    //     disk_filter,
+    //     mount_filter,
+    //     temp_filter,
+    //     net_filter,
+    // })
+
+    // let temp_type = app_config_fields.temperature_type.clone();
+    // let use_current_cpu_total = app_config_fields.use_current_cpu_total;
+    // let show_average_cpu = app_config_fields.show_average_cpu;
+
+    // let used_widgets = UsedWidgets {
+    //     use_cpu: used_widget_set.get(&Cpu).is_some() || used_widget_set.get(&BasicCpu).is_some(),
+    //     use_mem: used_widget_set.get(&Mem).is_some() || used_widget_set.get(&BasicMem).is_some(),
+    //     use_net: used_widget_set.get(&Net).is_some() || used_widget_set.get(&BasicNet).is_some(),
+    //     use_proc: used_widget_set.get(&Proc).is_some(),
+    //     use_disk: used_widget_set.get(&Disk).is_some(),
+    //     use_temp: used_widget_set.get(&Temp).is_some(),
+    //     use_battery: used_widget_set.get(&Battery).is_some(),
+    // };
+
+    let used_widgets = app::layout_manager::UsedWidgets {
+        use_cpu: true,
+        use_mem: true,
+        use_net: true,
+        use_proc: true,
+        use_disk: true,
+        use_temp: true,
+        use_battery: true,
+    };
+    let temp_type = data_harvester::temperature::TemperatureType::Fahrenheit;
+    let use_current_cpu_total = true;
+    let show_average_cpu = false;
+
+    let mut data_state = data_harvester::DataCollector::new(filters);
+    data_state.set_collected_data(used_widgets);
+    data_state.set_temperature_type(temp_type);
+    data_state.set_use_current_cpu_total(use_current_cpu_total);
+    data_state.set_show_average_cpu(show_average_cpu);
+
+    data_state.init();
+    futures::executor::block_on(data_state.update_data());
+
+    println!("{:#?}", data_state);
 }
